8eb0927535bc0de45fd09ee9c670c47d174235d2
Vicky Chen <www.vickychen123@gmail.com>
2024-10-22 23:47:47 -0400

    Added more

--- pscript.js
+++ pscript.js
@@ -2,60 +2,55 @@ const products = {
     'P1': {
         name: 'ShuttleCock',
         image: 'image/Shuttlecock.png',
-        description: 'This is just a shuttlecock.'
+        description: 'This is just a shuttlecock, for badminton.'
     },
     'P2': {
-        name: 'Product Name 2',
-        image: 'image/monkey.webp',
-        description: 'This is the description for Product 2.'
+        name: 'Slide Cap',
+        image: 'image/Slide Cap.png',
+        description: 'This is a Slide Cap, it is hollow in the middle'
     },
     'P3': {
         name: 'Mini Fan',
         image: 'image/Fan.png',
-        description: 'This is the description for the Mini Fan.'
+        description: 'It just a mini fan one of my first design'
     },
     'P4': {
         name: 'Water Heating Tube',
         image: 'image/Water Heating Tube.png',
-        description: 'This is the description for the 26 tooth Gear.'
+        description: 'Its just the case for a water heating tube.'
     },
     'P5': {
         name: 'Train',
         image: 'image/Train.png',
-        description: 'This is the description for the Train.'
+        description: 'An assembly made so the parts are connected to move in .'
     },
     'P6': {
         name: 'Hand Spin Fan',
-        image: '',
         description: 'This is the description for the Hand Spin Fan.',
         sketchfabEmbed: `<iframe title="Hand Spin Fan" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" src="https://sketchfab.com/models/e2dd23db441c45118223a2246e3ed733/embed"></iframe>`
     },
     'P7': {
         name: 'Robot Clipper',
-        image: '',
         description: 'This is the description for the Hand Spin Fan.',
         sketchfabEmbed:'<iframe title="Robot Gripper" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/a102254a53aa4a23b922c1253f0ec248/embed"> </iframe>'
     },
     'P8': {
-        name: '',
-        image: '',
-        description: 'This is the description for the Hand Spin Fan.',
+        name: 'Mounting Clip',
+        image: 'image/Holder.png',
+        description: 'Its a Mounting Clip, made it because my professor needed it. It fit perfectly. /n Diminsion:24.5mm x 24.5mm x 27.8mm'
     },
     'P9': {
         name: 'Jeep',
-        image: '',
         description: 'This is the description for the Hand Spin Fan.',
         sketchfabEmbed:'<iframe title="Toy Jeep" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/65b72126d9b245f4a65505f9883364b5/embed"> </iframe>'
     },
     'P10': {
         name: 'Selfie Stick',
-        image: '',
         description: 'This is the description for the Hand Spin Fan.',
         sketchfabEmbed:'<iframe title="Selfie Stick" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/77e7cf3393f643c19f98dcf5cbada453/embed"> </iframe>'
     },
     'P11': {
         name: 'Pen',
-        image: '',
         description: 'This is the description for the Hand Spin Fan.',
         sketchfabEmbed:'<iframe title="Pen" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true"allow="autoplay; fullscreen; xr-spatial-tracking"xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/4cd848a6eaec48a7a2327bd57bc8f2fa/embed?ui_infos=0&ui_watermark=0&ui_controls=1"</iframe>'
     }
